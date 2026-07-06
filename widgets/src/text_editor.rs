537c9bb466ca05c7795a54e6cadf06b7742c0804
mariinkys <kysgeneral.nrgua@slmail.me>
2026-07-06 19:48:42 +0200

    revert: 795eaebe243a43834b7b49de59a6bdd153e0a407

--- widgets/src/text_editor.rs
+++ widgets/src/text_editor.rs
@@ -822,7 +822,6 @@ where
                     }
                     Ime::Commit(text) => {
                         shell.publish(on_edit(Action::Edit(Edit::Paste(Arc::new(text)))));
-                        shell.capture_event();
                     }
                 },
                 Update::Binding(binding) => {
